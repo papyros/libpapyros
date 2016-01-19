5bfe8824cb0d7e823177424cf3ffe0264e330b4a
Michael Spencer <sonrisesoftware@gmail.com>
2016-01-18 21:18:32 -0600

    feat(qml): Add a QML module named "Papyros.Core"

---
+++ declarative/plugin.cpp
@@ -0,0 +1,29 @@
+/*
+ * libpapyros - A QML module and collection of classes used throughout Papyros
+ * Copyright (C) 2016 Michael Spencer <sonrisesoftware@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "plugin.h"
+
+#include <Papyros/KQuickConfig>
+
+void Plugin::registerTypes(const char *uri)
+{
+    // @uri Papyros.Core
+    Q_ASSERT(uri == QStringLiteral("Papyros.Core"));
+
+    qmlRegisterType<KQuickConfig>(uri, 0, 2, "KQuickConfig");
+}
